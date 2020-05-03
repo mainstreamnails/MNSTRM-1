f4c19bdd3eb45b0922c1266d5f9b569767b745bc
Ena Vu <ena.vu@charter.com>
2020-05-03 00:09:10 -0600

    team

--- js/all.js
+++ js/all.js
@@ -384,6 +384,13 @@
             },
             mainClass: "mfp-fade"
         });
+         // portfolio Item Lightbox desserts 				
+         $(".work-lightbox-link-elevate").magnificPopup({
+            gallery: {
+                enabled: true
+            },
+            mainClass: "mfp-fade"
+        });
         // portfolio Item Lightbox team 				
         $(".work-lightbox-link-team").magnificPopup({
             gallery: {
